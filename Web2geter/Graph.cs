0e9e2d36e8446c57854559282e2901e031876efa
K.Yuta <kayser250tr@gmail.com>
2018-02-21 21:38:58 +0900

    1st

---
+++ Web2geter/Graph.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Web2geter
+{
+    public partial class Graph : Form
+    {
+        public Graph()
+        {
+            InitializeComponent();
+        }
+
+        private void Graph_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
